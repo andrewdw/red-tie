9b14bd81744221564e6036e342a64ea817a6b0c3
Andrew Dillion-Walshe <andrewdillion6@gmail.com>
2016-11-14 15:35:59 -0500

    progress on observable storage

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,18 +14,11 @@ import { Router } from '@angular/router';
 import { Http, Headers } from '@angular/http';
 // storage
 const storage = require('electron-json-storage');
-import { isEmpty, isArray, isNumber } from 'lodash';
-
-import { config } from '../../config/config';
-
-// import { test } from '../../../observable-json-storage';
-
 const ojs = require("observable-json-storage");
-console.log('json store module', ojs)
 
 
-// var test2 = require('observable-json-storage')
-// console.log(test2)
+import { isEmpty, isArray, isNumber } from 'lodash';
+import { config } from '../../config/config';
 
 interface AccountToken {
   access_token: string;
@@ -134,93 +127,76 @@ export class AuthService {
   storeRefreshToken(token) {
     // take refresh token and put it in the current
   }
-  public storeNewAuthToken(token:AccountToken) {
+  public storeNewAuthToken(token:AccountToken):any {
     // attach current unix time to the token
     token.time = Math.round(new Date().getTime() / 1000);
-
     // get info for specific token and keep it in session
-    this.getAccountInfo(token).subscribe((accountInfo:any) => { // ********** add interface for info object
-      // do nothing if the account already exists in the accounts array
-      let accountExists = false;
-      for (let i = 0, len = this.accounts.length; i < len; i++) {
-        if (this.accounts[i].info.name === accountInfo.name) {
-          accountExists = true;
-          break;
+    return this.getAccountInfo(token)
+      .flatMap((accountInfo:any) => {
+        // do nothing if the account already exists in the accounts array
+        let accountExists = false;
+        for (let i = 0, len = this.accounts.length; i < len; i++) {
+          if (this.accounts[i].info.name === accountInfo.name) {
+            accountExists = true;
+            break;
+          }
         }
-      }
-      if (accountExists) {
-        alert(`${accountInfo.name} is already in your list of accounts.`);
-        return; // do nothing after alert
-      }
-
-      // pull from accounts settings, where this token will be stored
-      storage.get('accounts', (error, accountSettings) => {
-        let settingsToSave = { list: [], current: 0 } // our default save object
-        // if the settings aren't there alreadt
-        if ((isEmpty(accountSettings)) || (!accountSettings.hasOwnProperty('list')) || (!isArray(accountSettings.list))) {
-          accountSettings = settingsToSave;
+        if (accountExists) {
+          return Observable.throw(new Error(`${accountInfo.name} is already in your list of accounts.`))
         }
-        accountSettings.list.push(token); // push the new token to settings that we'll save
-        accountSettings.current = accountSettings.list.length - 1; // set current to the last item added
-        // store the values
-        storage.set('accounts', accountSettings, () => {
-          if (error) throw error;
-          // now that the token is saved to memory, add the user info to the token
-          token.info = accountInfo;
-          // push to session accounts list
-          this.accounts.push(token);
-          // set current account to the one we just added
-          this.currentAccount = this.accounts.length - 1;
-        })
+        // if account doesn't exist in local list, pull stored accounts
+        return ojs.get('accounts')
+          .flatMap((accountSettings) => {
+            let settingsToSave = { list: [], current: 0 } // our default save object
+            if ((!accountSettings) || (isEmpty(accountSettings)) || (!accountSettings.hasOwnProperty('list')) || (!isArray(accountSettings.list))) {
+              accountSettings = settingsToSave;
+            }
+            accountSettings.list.push(token); // push the new token to settings that we'll save
+            accountSettings.current = accountSettings.list.length - 1; // set current to the last item added
+            // save the settings to the file system
+            return ojs.set('accounts', accountSettings)
+              .do(() => {
+                // now that the token is saved to memory, add the user info to the token
+                token.info = accountInfo;
+                // push to session accounts list
+                this.accounts.push(token);
+                // set current account to the one we just added
+                this.currentAccount = this.accounts.length - 1;
+              })
+          })
       })
-    }, (err) => {
-      // error getting account info from reddit
-      if (err) {
-        alert('Something went wrong, please try again.');
-      }
-    })
   }
-  public getAllAccountsInfo(): void {
-    return ojs.get('accounts')
-      .map((accounts) {
-        console.log('accounts', accounts)
-      })
-
-    // return Observable.create((observer) => {
-
-    // }).first()
-
-    // pull account info from store
-    storage.get('accounts', (error, accountSettings) => { // *********** add interface
-      // if ((!isArray(accounts.list)) || (!isNumber(accounts.current))) {
-      //   // do something if the fetched values are not correct
-      //   // ***********
-      // } else {
-      if (error) {
-        // do something
-        return;
-      }
-      let arr = [];
-      for (let i = 0, len = accountSettings.list.length; i < len; i++) {
-        arr.push(this.getAccountInfo(accountSettings.list[i]))
-      }
-      // get array of all accounts' info concurrently
-      Observable.forkJoin(arr)
-      .subscribe((infoArray) => {
-        // loop through info array and push the accountSettings to the session object with the tokens
-        for (let i = 0, len = infoArray.length; i < len; i++) {
-          let obj = accountSettings.list[i];
-          obj.info = infoArray[i];
-          this.accounts.push(obj);
-        }
-        // also set the current account index
-        this.currentAccount = accountSettings.current;
-        // broadcast the complete event now
-        this.accountsUpdated.next();
-      }, (error) => {
-        // do something
-      });
-    });
+  public getAllAccountsInfo():any {
+    return Observable.create((observer) => {
+      let settings;
+      ojs.get('accounts')
+        .flatMap((accountSettings) => {
+          settings = accountSettings;
+          let arr = [];
+          for (let i = 0, len = settings.list.length; i < len; i++) {
+            arr.push(this.getAccountInfo(settings.list[i]))
+          }
+          return Observable.forkJoin(arr);
+        })
+        .subscribe((infoArray) => {
+          // we get an ordered list of account info returned, place them in session in order
+          // loop through info array and push the accountSettings to the session object with the tokens
+          for (let i = 0, len = infoArray.length; i < len; i++) {
+            let obj = settings.list[i];
+            obj.info = infoArray[i];
+            this.accounts.push(obj);
+          }
+          // also set the current account index
+          this.currentAccount = settings.current;
+          observer.next(true); // complete request
+          observer.complete();
+        },
+        (err) => {
+          alert('There was an error getting your account info. Please try again in a few minutes.');
+          observer.next(false);
+          observer.complete();
+        })
+    }).first();
   }
   private getAccountInfo(tokenObj:AccountToken) {
     // call http service with token defined
@@ -251,79 +227,79 @@ export class AuthService {
     // set current token object if it's not explicitly passed
     tokenObj = (tokenObj) ? tokenObj : this.accounts[this.currentAccount];
     console.log('is barer expired', this.bearerIsExpired(tokenObj), tokenObj)
-    if (this.bearerIsExpired(tokenObj)) {
-      console.log('THIS BEARER expired')
-      // we need to chain the refresh function to the get request
-      let oldTokenObj = tokenObj; // rename it to limit confusion later
-      let accountSettings, tokenIndex = 0;
-      let getSettingsObservable:any = Observable.bindCallback(storage.get);
-      let setSettingsObservable:any = Observable.bindCallback(storage.set);
-      return getSettingsObservable('accounts').flatMap((d) => {
-        // TODO: do something with the error (d[0])
-        // store the account settings into our local variable declared above
-        accountSettings = d[1];
-        // now look for refresh the token also declared above
-        return this.refreshSpecificToken(oldTokenObj);
-      })
-      .flatMap((token) => {
-        // when we store the token later we need to know which index item it is.. so lets search here
-        for (let i = 0, len = accountSettings.list.length; i < len; i++) {
-          // compare the old token with the access settings
-          if (accountSettings.list[i].access_token === oldTokenObj.access_token) {
-            console.log('found index')
-            tokenIndex = i; // set index
-            break; // end loop
-          }
-        }
-        // store token in local variable
-        // attach current unix time to the new token
-        token.time = Math.round(new Date().getTime() / 1000);
-        // NOTE: The new token is identical to the user token except it does NOT have the `refresh_token` key present...
-        // so we'll add it to the new token from the old
-        token.refresh_token = oldTokenObj.refresh_token;
-        // put the new token back to our accounts list
-        accountSettings.list[tokenIndex] = token;
-        // if the session accounts list is built, also put the relevant info there
-        if (this.accounts.length > 0) {
-          let tempOldObj = this.accounts[tokenIndex];
-          // we create a temp new object because we dont't want the account info (username, etc..) stored in the localSorage
-          let tempNewObj = Object.create(token);
-          tempNewObj.info = tempOldObj.info;
-          this.accounts[tokenIndex] = tempNewObj;
-        }
-        console.log('----------')
-        console.log('new token object', token)
-        console.log('settings to save', accountSettings)
-        console.log('this token index', tokenIndex)
-        console.log('session accounts', this.accounts)
-        console.log('----------')
-        // save the token
-        // return setSettingsObservable('accounts', accountSettings);
-      })
-      .map((d) => {
-        // continue with data request
-        console.log('---------------')
-        console.log('saved', d);
-        console.log(accountSettings.list[tokenIndex])
-        console.log('---------------')
-        let headers = new Headers();
-        this.createGETHeader(headers, accountSettings.list[tokenIndex]);
+    // if (this.bearerIsExpired(tokenObj)) {
+    //   console.log('THIS BEARER expired')
+    //   // we need to chain the refresh function to the get request
+    //   let oldTokenObj = tokenObj; // rename it to limit confusion later
+    //   let accountSettings, tokenIndex = 0;
+    //   let getSettingsObservable:any = Observable.bindCallback(storage.get);
+    //   let setSettingsObservable:any = Observable.bindCallback(storage.set);
+    //   return getSettingsObservable('accounts').flatMap((d) => {
+    //     // TODO: do something with the error (d[0])
+    //     // store the account settings into our local variable declared above
+    //     accountSettings = d[1];
+    //     // now look for refresh the token also declared above
+    //     return this.refreshSpecificToken(oldTokenObj);
+    //   })
+    //   .flatMap((token) => {
+    //     // when we store the token later we need to know which index item it is.. so lets search here
+    //     for (let i = 0, len = accountSettings.list.length; i < len; i++) {
+    //       // compare the old token with the access settings
+    //       if (accountSettings.list[i].access_token === oldTokenObj.access_token) {
+    //         console.log('found index')
+    //         tokenIndex = i; // set index
+    //         break; // end loop
+    //       }
+    //     }
+    //     // store token in local variable
+    //     // attach current unix time to the new token
+    //     token.time = Math.round(new Date().getTime() / 1000);
+    //     // NOTE: The new token is identical to the user token except it does NOT have the `refresh_token` key present...
+    //     // so we'll add it to the new token from the old
+    //     token.refresh_token = oldTokenObj.refresh_token;
+    //     // put the new token back to our accounts list
+    //     accountSettings.list[tokenIndex] = token;
+    //     // if the session accounts list is built, also put the relevant info there
+    //     if (this.accounts.length > 0) {
+    //       let tempOldObj = this.accounts[tokenIndex];
+    //       // we create a temp new object because we dont't want the account info (username, etc..) stored in the localSorage
+    //       let tempNewObj = Object.create(token);
+    //       tempNewObj.info = tempOldObj.info;
+    //       this.accounts[tokenIndex] = tempNewObj;
+    //     }
+    //     console.log('----------')
+    //     console.log('new token object', token)
+    //     console.log('settings to save', accountSettings)
+    //     console.log('this token index', tokenIndex)
+    //     console.log('session accounts', this.accounts)
+    //     console.log('----------')
+    //     // save the token
+    //     // return setSettingsObservable('accounts', accountSettings);
+    //   })
+    //   .map((d) => {
+    //     // continue with data request
+    //     console.log('---------------')
+    //     console.log('saved', d);
+    //     console.log(accountSettings.list[tokenIndex])
+    //     console.log('---------------')
+    //     let headers = new Headers();
+    //     this.createGETHeader(headers, accountSettings.list[tokenIndex]);
 
-        //***************
-        setTimeout(()=> {
-          storage.get('accounts', (error, accountSettings) => { // *********** add interface
-            console.log('got settings', accountSettings)
-          }, 6000)
-        })
-        //**************
+    //     //***************
+    //     setTimeout(()=> {
+    //       storage.get('accounts', (error, accountSettings) => { // *********** add interface
+    //         console.log('got settings', accountSettings)
+    //       }, 6000)
+    //     })
+    //     //**************
 
-        return this.http.get(url, { headers })
-      })
-    } else {
+    //     return this.http.get(url, { headers })
+    //   })
+    // } else {
       let headers = new Headers();
       this.createGETHeader(headers, tokenObj);
       return this.http.get(url, { headers })
-    }
+    // }
   }
   /////////////////
   // AUTH GUARDS //
@@ -332,8 +308,9 @@ export class AuthService {
     return Observable.create((observer) => {
       ojs.has('accounts')
       .flatMap((keyExists) => {
+        // if token and no info, get info
         if (keyExists && (this.accounts.length === 0)) {
-          return Observable.of(true);
+          return this.getAllAccountsInfo();
         } else if (keyExists) {
           return Observable.of(true);
         } else {
@@ -343,46 +320,10 @@ export class AuthService {
         }
       })
       .subscribe((res) => {
-        console.log(res)
-        // if token and no info, get info
         observer.next(res);
         observer.complete();
       })
-      // .subscribe((keyExists) => {
-      //   // if token and no info, get info
-      //   if (keyExists && (this.accounts.length === 0)) {
-      //     console.log('has key')
-      //     return observer.next(true);
-      //   } else if (keyExists) {
-      //     return observer.next(true);
-      //   } else {
-      //     // else reject to accounts page
-      //     this.router.navigate(['/accounts']);
-      //     return observer.next(false);
-      //   }
-      // })
     }).first();
-    // console.log(this.accounts, this.currentAccount)
-    // let haskeyObservable:any = Observable.bindCallback(storage.has);
-    // let key = haskeyObservable('accounts');
-    // return key.map((d) => {
-    //   let keyExists = d[1];
-    //   // if token and no info, get info
-    //   if (keyExists && (this.accounts.length === 0)) {
-    //     // run the function that will collect user info for tokens
-    //     // main component should run loading sequence till this function fires a finished event
-    //     this.getAllAccountsInfo();
-    //     console.log('getting all accounts info')
-    //     return true;
-    //   // if token (imply that there is info), continue
-    //   } else if (keyExists) {
-    //     return true;
-    //   } else {
-    //     // else reject to accounts page
-    //     this.router.navigate(['/accounts']);
-    //     return false;
-    //   }
-    // })
   }
 
 }
